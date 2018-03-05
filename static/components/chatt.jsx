c2b99a0123b7df656fa7e601a77cf044fbcc5d48
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-03-05 15:05:33 +0100

    vet ej

--- static/components/chatt.jsx
+++ static/components/chatt.jsx
@@ -1,12 +1,24 @@
 var React = require('react');
 
 class Chatt extends React.Component {
-	render(){
-		return <div>
-      <p>test</p>
-      <input></input>
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    };
+  }
+  render() {
+    return <div>
+      <p>{this.state.text}</p>
+      <input onChange={function(event){
+          this.setState({text: event.target.value})
+        }}></input>
+
+      <button onClick={function() {
+
+        }}></button>
     </div>
-	}
+  }
 }
 
 module.exports = Chatt;
