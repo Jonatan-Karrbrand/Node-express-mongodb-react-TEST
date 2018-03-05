5db72cf7c9898eebc5f728f010fa22c77e7791cc
lingvald <lukasingjonson@gmail.com>
2018-03-05 13:45:39 +0100

    bytt namn till jsx

---
+++ static/index.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var HelloWorld = require('./components/helloworld.jsx');
+
+ReactDOM.render(<HelloWorld></HelloWorld>, document.getElementById('app'));
