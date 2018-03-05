2054e5a00d8b4a6cab5f03867e4649accdcf4ed2
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-03-05 14:36:59 +0100

    La till chatt component och satte allt i en div i ReactDom render

---
+++ static/components/chatt.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+class Chatt extends React.Component {
+	render(){
+		return <div>
+      <p>test</p>
+      <input></input>
+    </div>
+	}
+}
+
+module.exports = Chatt;
--- static/index.jsx
+++ static/index.jsx
@@ -2,4 +2,9 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 /*var HelloWorld = require('./components/helloworld.jsx');*/
 var LoginComponent = require('./components/login.jsx');
-ReactDOM.render(<LoginComponent></LoginComponent>, document.getElementById('app'));
+var ChattComponent = require('./components/chatt.jsx');
+ReactDOM.render(<div>
+    <LoginComponent></LoginComponent>
+    <ChattComponent></ChattComponent>
+  </div>
+  ,document.getElementById('app'));
