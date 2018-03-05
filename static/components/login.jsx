8590a37f24ac1a75bf7909ad6f16fb5b54810ffd
lingvald <lukasingjonson@gmail.com>
2018-03-05 14:53:16 +0100

    L

--- static/components/login.jsx
+++ static/components/login.jsx
@@ -2,17 +2,12 @@ var React = require('react');
 
 class LoginComponent extends React.Component {
 	render(){
-		var inputUsername = <input onChange={function(){
-					var v
-					console.log(this.state)}} type="text"></input>;
-		var inputPassword = <input type="password"></input>;
-		var button = <button>Logga in</button>;
 		return <div id="login-container">
 			<h2>Logga in</h2>
-				<div id="inputs-container">
-					<form>
-					{inputUsername}
-					{inputPassword}
+				<div>
+					<form id="inputs-container">
+					<input onChange={function(event){console.log(event.target.value);}}></input>
+					<input type="password"></input>;
 					</form>
 				</div>
 		</div>
