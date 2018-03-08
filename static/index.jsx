0668e53fcd0b4b99a03caedb53fa9b0b92e134ea
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-03-08 12:18:30 +0100

    Saker

--- static/index.jsx
+++ static/index.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-/*var HelloWorld = require('./components/helloworld.jsx');*/
+
 var LoginComponent = require('./components/login.jsx');
 var ChattComponent = require('./components/chatt.jsx');
 var RegisterComponent = require('./components/register.jsx');
