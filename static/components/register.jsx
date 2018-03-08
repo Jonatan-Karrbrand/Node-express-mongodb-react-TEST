9a69db942735ff0b8ef34e176dd60186a8d5a409
Kristian Jimmefors <kristian.jimmefors@gmail.com>
2018-03-08 09:10:50 +0100

    Gjorde lite mer saker till registreringens front end

--- static/components/register.jsx
+++ static/components/register.jsx
@@ -2,20 +2,21 @@ var React = require('react');
 
 class Register extends React.Component {
 	render(){
-		return <div>
-      <h1>Registrering</h1>
-      <div className="regname">
-        <p>Namn:</p>
-        <input type="text"></input>
-      </div>
-      <div className="regpassword">
-        <p>Lösenord:</p>
-        <input type="text"></input>
-      </div>
-      <div className="regmail">
+		return <div id="register-container">
+      <h2>Registrering</h2>
+      <div id="register-mail">
         <p>E-post:</p>
         <input type="text"></input>
       </div>
+      <div id="register-name">
+        <p>Namn:</p>
+        <input type="text" onChange={ function(event){ console.log(event.target.value)}}></input>
+      </div>
+      <div id="register-passwords">
+        <p>Lösenord:</p>
+        <input type="password" onChange={ function(event){ console.log(event.target.value)}}></input>
+      </div>
+      <input type="button" value="Registrera"></input>
     </div>
 	}
 }
--- static/index.jsx
+++ static/index.jsx
@@ -2,4 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 /*var HelloWorld = require('./components/helloworld.jsx');*/
 var LoginComponent = require('./components/login.jsx');
-ReactDOM.render(<LoginComponent></LoginComponent>, document.getElementById('app'));
+var Register = require('./components/register.jsx');
+
+ReactDOM.render(<div><LoginComponent></LoginComponent><Register></Register></div>, document.getElementById('app'));
