5305bf3f376218e99167b4092d0631246bc4277f
Jonatan-Karrbrand <jonatan_96__@hotmail.com>
2018-03-08 11:56:53 +0100

    En början på chatt komponenten. Man kan skicka ens meddelande till databasen.

--- static/components/chatt.jsx
+++ static/components/chatt.jsx
@@ -10,13 +10,23 @@ class Chatt extends React.Component {
   render() {
     return <div>
       <p>{this.state.text}</p>
-      <input onChange={function(event){
-          this.setState({text: event.target.value})
-        }} value={this.state.text}></input>
+      <input onChange={event => this.setState({text: event.target.value})}></input>
 
-      <button onClick={function() {
-
-        }}></button>
+      <button onClick={() => fetch('http://localhost:3000/messages', {
+        body: '{"messages":"' + this.state.text + '"}',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: 'POST'
+      }).then(function(response) {
+        console.log(response)
+        return response.json();
+      }).then(function(result) {
+        console.log(result)
+      }),
+      () => {
+        alert('skitbra ')
+      }}>Send</button>
     </div>
   }
 }
--- static/index.jsx
+++ static/index.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require('react-dom');
 /*var HelloWorld = require('./components/helloworld.jsx');*/
 var LoginComponent = require('./components/login.jsx');
 var ChattComponent = require('./components/chatt.jsx');
+
+
 ReactDOM.render(<div>
     <LoginComponent></LoginComponent>
     <ChattComponent></ChattComponent>
